9e461bc9b1912b405feee052c94be8294fcfa61f
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2022-02-18 14:02:08 -0300

    Requisito 11

---
+++ src/pages/Feedback.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <p data-testid="feedback-text">Feedback</p>
+    );
+  }
+}
+
+export default Feedback;
