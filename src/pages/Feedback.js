1e65bdbeb91992a4081c31c70ded215866e5038b
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2022-02-18 15:16:55 -0300

    requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Feedback extends Component {
   render() {
+    const MIN = 3;
+    const { assertions } = this.props;
     return (
-      <p data-testid="feedback-text">Feedback</p>
+      <section data-testid="feedback-text">
+        { assertions < MIN
+          ? <p>Could be better...</p>
+          : <p>Well Done!</p>}
+      </section>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = ({ player }) => ({
+  assertions: player.assertions,
+  score: player.score,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+  score: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Feedback);
