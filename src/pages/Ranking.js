bc0ee3510ead547bd42ad5d612d84a5bbdbda164
Rosalia Padoin Oliveira <rosalia@rosalia-VPCEH27FX>
2022-02-18 18:53:28 -0300

    requisito 17

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Tela Inicial
+        </button>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.string,
+}.isRequired;
 
 export default Ranking;
